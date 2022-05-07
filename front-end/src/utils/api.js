238e02b7465b79ee47d6f9b61cb534eaf835ad2b
surya chandrasekar <rainsinseptember@gmail.com>
2022-05-06 21:05:58 -0500

    clean

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,8 +5,7 @@
  import formatReservationDate from "./format-reservation-date";
  import formatReservationTime from "./format-reservation-date";
  
- const API_BASE_URL =
-   REACT_APP_API_BASE_URL || "https://suryarestaurant.herokuapp.com/";
+ const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://suryarestaurant.herokuapp.com/";
  
  /**
  * Defines the default headers for these functions to work with `json-server`
